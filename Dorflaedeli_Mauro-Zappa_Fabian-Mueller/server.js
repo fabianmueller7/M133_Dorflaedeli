5e9a0af3554f614be1feddcfc79fca1dbf01f764
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-01-08 13:24:00 +0100

    project folder added

---
+++ Dorflaedeli_Mauro-Zappa_Fabian-Mueller/server.js
@@ -0,0 +1,13 @@
+'use strict'
+import {Application} from "https://deno.land/x/oak@v6.3.1/mod.ts";
+import { apiRoutes } from "./backend/app.js";
+import { frontendFiles } from "./backend/fileserver.js";
+
+const app = new Application();
+
+app.use(apiRoutes);
+app.use(frontendFiles);
+
+app.listen({port: 8000});
+
+console.log("listening on port: 8000");
