300daed991a05caebd892abaa15e842c3ef84e80
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-01-08 15:04:44 +0100

    api started

--- Dorflaedeli_Mauro-Zappa_Fabian-Mueller/backend/app.js
+++ Dorflaedeli_Mauro-Zappa_Fabian-Mueller/backend/app.js
@@ -1,21 +1,10 @@
 'use strict'
 import {Router} from 'https://deno.land/x/oak@v6.3.1/mod.ts';
-import {v4} from 'https://deno.land/std@0.77.0/uuid/mod.ts';
 
-
-//status
-//1 = ToDo
-//2 = Progress
-//3 = Done
-let list = [
-    {id: v4.generate(), name: "Meeting for Kanban Project at 17.50.", description: "Meeting Kanban", status: 3},
-    {id: v4.generate(), description: "Study Page 185+186 in Dornbader.", title: "Physics Test!", status: 1 },
-    {id: v4.generate(), description: "Mathematics Page 19, Number 3+4.", title: "Homework", status: 2},
-];
 
 const router = new Router();
 
 router
-.get("/api", context => context.response.body = list);
+.get("/api/allarticles", async context => context.response.body = await JSON.parse(Deno.readTextFileSync('./products.json')));
 
 export const apiRoutes = router.routes();
