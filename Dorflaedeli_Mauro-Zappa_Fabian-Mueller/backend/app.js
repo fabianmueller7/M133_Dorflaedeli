52d77eba8b221e72eca089218fa4baab93349c00
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-01-09 13:49:49 +0100

    products.json relocated

--- Dorflaedeli_Mauro-Zappa_Fabian-Mueller/backend/app.js
+++ Dorflaedeli_Mauro-Zappa_Fabian-Mueller/backend/app.js
@@ -5,11 +5,11 @@ import {Router} from 'https://deno.land/x/oak@v6.3.1/mod.ts';
 const router = new Router();
 
 router
-.get("/api/allArticles", async context => context.response.body = await JSON.parse(await Deno.readTextFile('products.json')))
+.get("/api/allArticles", async context => context.response.body = await JSON.parse(await Deno.readTextFile('./backend/products.json')))
 .get("/api/articleById", async context => {
     const item =  await context.request.body({type: "json"}).value;
     console.log(item);
-    context.response.body = await JSON.parse(await Deno.readTextFile('products.json')).filter(function (el){return el.id == item.id})
+    context.response.body = await JSON.parse(await Deno.readTextFile('./backend/products.json')).filter(function (el){return el.id == item.id})
 });
 
 export const apiRoutes = router.routes();
