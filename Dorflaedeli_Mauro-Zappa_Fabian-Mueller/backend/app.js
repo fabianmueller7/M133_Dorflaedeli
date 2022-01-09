3d3df427d221a76db8c964c67fcd12de426c45a1
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-01-09 13:48:21 +0100

    API AllArticles and articleById implemented

--- Dorflaedeli_Mauro-Zappa_Fabian-Mueller/backend/app.js
+++ Dorflaedeli_Mauro-Zappa_Fabian-Mueller/backend/app.js
@@ -5,6 +5,11 @@ import {Router} from 'https://deno.land/x/oak@v6.3.1/mod.ts';
 const router = new Router();
 
 router
-.get("/api/allarticles", async context => context.response.body = await JSON.parse(Deno.readTextFileSync('./products.json')));
+.get("/api/allArticles", async context => context.response.body = await JSON.parse(await Deno.readTextFile('products.json')))
+.get("/api/articleById", async context => {
+    const item =  await context.request.body({type: "json"}).value;
+    console.log(item);
+    context.response.body = await JSON.parse(await Deno.readTextFile('products.json')).filter(function (el){return el.id == item.id})
+});
 
 export const apiRoutes = router.routes();
